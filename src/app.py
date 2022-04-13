51fbe2d8760d6bc3a599deb24638e79357bf05f9
Olga-Shabatina <OlgaShabatina@gmail.com>
2022-04-13 16:16:13 +0200

    src: added render_template

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
-from flask import Flask # importing the flask class
+from flask import Flask, render_template # importing the flask class
 app = Flask(__name__) # creating an instance of the Flask class
  
 @app.route('/') # The primary url for our application
 def hello_world(): # This method returns 'Flask Dockerized', which is displayed in our browser.
-    return 'Hello! I am dockerized Flask web framework'
+    return render_template('index.html')
+    #return 'Hello! I am dockerized Flask web framework'
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0') # This statement starts the server on your local machine.
